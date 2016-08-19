d9bdb2346bf610824e7c6bfc3d431cf8cc866a04
Marco Chiappetta <lambdacomplete@gmail.com>
2016-08-19 11:53:59 +0200

    Version bump to address urlpatterns update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-easygeoip',
-    version='0.0.2',
+    version='0.0.3',
     packages=['easygeoip'],
     include_package_data=True,
     license='MIT License',
