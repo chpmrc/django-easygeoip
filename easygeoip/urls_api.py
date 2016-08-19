24045cd16a862ebd31f4a88a733a05bf2aff03a5
Marco Chiappetta <lambdacomplete@users.noreply.github.com>
2016-08-19 11:33:30 +0200

    Upgrade to new urlpatterns format

--- easygeoip/urls_api.py
+++ easygeoip/urls_api.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 # API URLs
 from .views import LocationFromIpView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^location/(?P<ip_address>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))/$', LocationFromIpView.as_view(),
         name='geoip-explicit-ip-view'),
     url(r'^location/$', LocationFromIpView.as_view(), name='geoip-implicit-ip-view')  # Take IP addr from request
-)
+]
